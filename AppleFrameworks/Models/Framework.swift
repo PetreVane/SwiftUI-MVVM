4791c16f663c16216226164136ef57ff781e20bb
Petre Vane <petre.vane@gmail.com>
2020-11-12 11:33:00 +0100

    project setup; added resources

---
+++ AppleFrameworks/Models/Framework.swift
@@ -0,0 +1,18 @@
+//
+//  Framework.swift
+//  AppleFrameworks
+//
+//  Created by Petre Vane on 11/11/2020.
+//
+
+import Foundation
+
+
+struct Framework {
+    
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
+
